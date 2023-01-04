1cb1cd92327b7bae1a912bf58aac3b807010d6e7
Alberto Bautista <alberto.bautista@capgemini.com>
2023-01-04 16:50:32 -0600

    Add CalendarEvent component

---
+++ src/calendar/components/CalendarEvent.jsx
@@ -0,0 +1,9 @@
+export const CalendarEvent = ({ event }) => {
+  const { title, user } = event;
+  return (
+    <>
+      <strong>{user.name}</strong>
+      <span> - {title}</span>
+    </>
+  );
+};
