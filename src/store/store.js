62cb21a762da0c1030f3c0a5f42e7854532ac128
Alberto Bautista <alberto.bautista@capgemini.com>
2023-01-05 15:55:26 -0600

    Add authSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/authSlice";
 import { calendarSlice } from "./calendar/calendarSlice";
 import { uiSlice } from "./ui/uiSlice";
 
 export const store = configureStore({
   reducer: {
-    ui: uiSlice.reducer,
+    auth: authSlice.reducer,
     calendar: calendarSlice.reducer,
+    ui: uiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
